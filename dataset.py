3f339381dbb1ba0000783f2d37538bce0295df98
PanPapag <sdi1700115@di.uoa.gr>
2022-06-23 17:58:16 +0300

    adding custom pytorch dataset class

---
+++ dataset.py
@@ -0,0 +1,13 @@
+import torch
+import torch.utils.data as data
+
+class Dataset(data.Dataset):
+    def __init__(self, X_data, y_data):
+        self.X_data = X_data
+        self.y_data = y_data
+        
+    def __getitem__(self, index):
+        return self.X_data[index], self.y_data[index]
+        
+    def __len__ (self):
+        return len(self.X_data)
